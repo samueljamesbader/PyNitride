7dfcd071d7963bd1dc220eb50cabe0c9a2a21af8
samueljamesbader <samuel.james.bader@gmail.com>
2017-01-04 02:19:23 -0500

    Added --plots to pytest, fixed pytest import order issues

--- poissolve/tests/runtests.py
+++ poissolve/tests/runtests.py
@@ -5,5 +5,13 @@ Created on Tue Jan  3 10:50:17 2017
 @author: sam
 """
 
+# Put nothing before this
+# because all other lines should be run *after* pytest.main
 import pytest
-pytest.main()
+if __name__=='__main__':
+    pytest.main()
+
+plots = pytest.mark.skipif(
+    not pytest.config.getoption("--plots"),
+    reason="need --plots option to run"
+)
