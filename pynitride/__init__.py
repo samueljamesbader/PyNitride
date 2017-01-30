1afd12051f5fa97f705102ed3ca130e7c8df73e1
samueljamesbader <samuel.james.bader@gmail.com>
2017-01-29 21:55:12 -0500

    ParamDB supports new fancy syntax and output of Pint quantities

--- pynitride/__init__.py
+++ pynitride/__init__.py
@@ -1,8 +1,18 @@
 import os.path
 ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__),'..'))
 
-import pynitride.paramdb
-#from pynitride.paramdb import ParamDB, Material, to_unit, parse, convenient_constants
-#for const in convenient_constants: globals()[const]=getattr(pynitride.paramdb,const)
+from pynitride.paramdb import ParamDB, Material, Value, parr
+to_unit=Value.to_unit
+parse=Value.parse
+del Value
+
+convenient_constants=["hbar","c","m_e","angstrom","nm","um","mm","cm","mV","V","kV","MV","meV","eV","keV","MeV","epsilon_0"]
+for const in convenient_constants:
+    globals()[const]=parse(const)
+q=parse('e')
+T=300
+kT=parse('k')*T
+del convenient_constants
+
 from pynitride.poissolve.mesh.structure import Mesh, SubMesh, EpiStack
 from pynitride.poissolve.mesh.functions import MaterialFunction, MidFunction, PointFunction, ConstantFunction, RegionFunction
