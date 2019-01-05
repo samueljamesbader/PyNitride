fefe621cbaeaedf1b800609057b23b5cc056d9be
Samuel James Bader <samuel.james.bader@gmail.com>
2019-01-05 06:37:51 +0000

    Pool globals

--- pynitride/machine.py
+++ pynitride/machine.py
@@ -2,6 +2,9 @@ import os
 from multiprocessing import cpu_count
 from multiprocessing import Pool as _ProcessPool
 from multiprocessing.dummy import Pool as _ThreadPool
+from operator import itemgetter
+from threading import Lock
+from contextlib import contextmanager
 
 class Pool():
 
@@ -22,6 +25,8 @@ class Pool():
             return
 
         cls._kwargs={'globalthreads':globalthreads,'globalprocesses':globalprocesses,'cextthread':cextthread}
+        cls._globs={}
+        cls._globlck=Lock()
         if cextthread is not None:
             os.environ["OMP_NUM_THREADS"] = str(cextthread)
             os.environ["OPENBLAS_NUM_THREADS"] = str(cextthread)
@@ -30,16 +35,18 @@ class Pool():
             os.environ["NUMEXPR_NUM_THREADS"] = str(cextthread)
 
         if globalprocesses>1:
-            cls._procpool=_ProcessPool(processes=globalprocesses)
+            cls._procpool=_ProcessPool(processes=globalprocesses,maxtasksperchild=10)
         else: cls._procpool=FakePool()
         if globalthreads>1:
             cls._thrdpool=_ThreadPool(processes=globalprocesses)
         else: cls._thrdpool=FakePool()
 
     @classmethod
-    def process_pool(cls):
+    def process_pool(cls,new=False):
         if not hasattr(cls,'_kwargs'):
             cls.configure()
+        elif new:
+            cls._refresh_pool()
         return cls._procpool
 
     @classmethod
@@ -47,6 +54,36 @@ class Pool():
         if not hasattr(cls,'_kwargs'):
             cls.configure()
         return cls._thrdpool
+
+    @classmethod
+    def _refresh_pool(cls):
+        globalprocesses=itemgetter('globalprocesses')(cls._kwargs)
+        if globalprocesses>1:
+            cls._procpool.close()
+            cls._procpool.join()
+            cls._procpool=_ProcessPool(processes=globalprocesses,maxtasksperchild=10)
+    
+    @classmethod
+    @contextmanager
+    def reference(cls,*args):
+        if not hasattr(cls,'_kwargs'):
+            cls.configure()
+        with cls._globlck:
+            n=range(len(cls._globs),len(cls._globs)+len(args))
+            for i,ni in enumerate(n):
+                cls._globs[ni]=args[i]
+
+        #cls._refresh_pool()
+        if len(args)==1: yield list(n)[0]
+        else: yield list(n)
+
+
+        with cls._globlck:
+            for ni in n:
+                del cls._globs[ni]
+    @classmethod
+    def dereference(cls,*args):
+        return itemgetter(*args)(cls._globs)
 
 class FakePool():
     def starmap(self,func,iterable,chunksize=1):
