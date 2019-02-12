c60840f9016b9897f01bc01a8c48bfb17e511195
Samuel James Bader <samuel.james.bader@gmail.com>
2019-02-12 05:49:21 +0000

    _no_parallel to class

--- pynitride/machine.py
+++ pynitride/machine.py
@@ -43,7 +43,7 @@ class Pool():
             cls._thrdpool=_ThreadPool(processes=globalprocesses)
         else: cls._thrdpool=FakePool()
 
-        cls._no_parallel=False
+    _no_parallel=False
 
     @classmethod
     def process_pool(cls,new=False):
@@ -195,7 +195,8 @@ def glob_store_attributes(*attrs):
         def __del__(self):
             odel(self)
             for k,key in self._globkeys.items():
-                glob_remove(key)
+                try: glob_remove(key)
+                except: log("Trouble removing key",level='debug')
 
         # Add this new __del__ to the class
         cls.__del__=__del__
