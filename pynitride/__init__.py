7d0eed776ec8feb44db4d2bface4d6d33e26354a
Samuel James Bader <samuel.james.bader@gmail.com>
2026-03-06 22:58:45 -0800

    Moved to pyproject.toml

--- pynitride/__init__.py
+++ pynitride/__init__.py
@@ -1,10 +1,28 @@
 import os.path
 from configparser import ConfigParser
+from multiprocessing import get_all_start_methods
 
 # Needed for reading configuration
-ROOT_DIR= os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)),'..'))
-config=ConfigParser()
-config.read(os.path.join(ROOT_DIR,"config.ini"))
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..'))
+
+def _make_default_config(path):
+    cp = ConfigParser()
+    parallel = 'fork' in get_all_start_methods()
+    cp.add_section("parallelism")
+    cp.set("parallelism", "globalthreads", "cpu_count")
+    cp.set("parallelism", "globalprocesses", "cpu_count" if parallel else "1")
+    cp.set("parallelism", "cextthread", "1" if parallel else "DEFAULT")
+    cp.add_section("logging")
+    cp.set("logging", "level", "info")
+    with open(path, 'w') as f:
+        cp.write(f)
+
+_config_path = os.path.join(ROOT_DIR, "config.ini")
+if not os.path.exists(_config_path):
+    _make_default_config(_config_path)
+
+config = ConfigParser()
+config.read(_config_path)
 """ The directory of the PyNitride project"""
 
 # Logging is needed everywhere and doesn't require anything (eg numpy)
