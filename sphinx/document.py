f8b9e474918094073dd1ea9538a534b38958b6cc
Samuel James Bader <samuel.james.bader@gmail.com>
2026-03-14 12:13:13 -0700

    updated contributing, check doc build in test job

--- sphinx/document.py
+++ sphinx/document.py
@@ -5,7 +5,7 @@ import pathlib
 sphinx_dir = pathlib.Path(__file__).parent
 root = sphinx_dir.parent
 
-subprocess.run(["sphinx-apidoc", "-o", str(sphinx_dir/"auto"), str(root/"src"/"pynitride"), "-f", "-e"])
-subprocess.run(["sphinx-build", "-b", "html", str(sphinx_dir), str(root / "docs" / "html")])
+subprocess.run(["sphinx-apidoc", "-o", str(sphinx_dir/"auto"), str(root/"src"/"pynitride"), "-f", "-e"],check=True)
+subprocess.run(["sphinx-build", "-b", "html", str(sphinx_dir), str(root / "docs" / "html")],check=True)
 shutil.copy(sphinx_dir / "index_redirect.html", root / "docs" / "html" / "index.html")
 shutil.copy(root / "docs" / "html" / ".nojekyll", root / "docs" / ".nojekyll")
